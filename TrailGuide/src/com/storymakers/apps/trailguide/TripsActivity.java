d9092fc386a7648d7ba9285b420b70cbffa8134c
Vishal Patel <vpatel.apple@gmail.com>
2014-09-30 12:12:02 -0700

    Adding a new android project.

---
+++ TrailGuide/src/com/storymakers/apps/trailguide/TripsActivity.java
@@ -0,0 +1,15 @@
+package com.storymakers.apps.trailguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TripsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips);
+    }
+}
