00c5e1c5d8b6f5318785ddfc21480e63aebaea92
Vandana Bachani <vandana.bvj@gmail.com>
2014-10-30 00:29:22 -0700

    ref stories in create

--- TrailGuide/src/com/storymakers/apps/trailguide/adapters/StoryPostAdapter.java
+++ TrailGuide/src/com/storymakers/apps/trailguide/adapters/StoryPostAdapter.java
@@ -1,15 +1,10 @@
 package com.storymakers.apps.trailguide.adapters;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -26,6 +21,7 @@ import android.widget.TextView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 import com.parse.ParseGeoPoint;
 import com.storymakers.apps.trailguide.R;
 import com.storymakers.apps.trailguide.TrailGuideApplication;
@@ -97,9 +93,7 @@ public class StoryPostAdapter extends ArrayAdapter<TGPost> {
 		}
 
 		if (type == TGPost.PostType.PHOTO.getNumVal()) {
-			TextView tvPostDateTime = (TextView) convertView
-					.findViewById(R.id.tvPostDateTime);
-			tvPostDateTime.setText(post.getFormattedCreateTime());
+			setTimeInclude(convertView, post);
 			ImageView ivPostPhoto = (ImageView) convertView
 					.findViewById(R.id.ivPostPhoto);
 			ivPostPhoto.setImageResource(android.R.color.transparent);
@@ -162,9 +156,7 @@ public class StoryPostAdapter extends ArrayAdapter<TGPost> {
 			}
 		}
 		if (type == TGPost.PostType.NOTE.getNumVal()) {
-			TextView tvPostDateTime = (TextView) convertView
-					.findViewById(R.id.tvPostDateTime);
-			tvPostDateTime.setText(post.getFormattedCreateTime());
+			setTimeInclude(convertView, post);
 			TextView tvPostNote = (TextView) convertView
 					.findViewById(R.id.tvPostNote);
 			if (tvPostNote != null && post.getNote() != null) {
@@ -175,6 +167,7 @@ public class StoryPostAdapter extends ArrayAdapter<TGPost> {
 		}
 		if (type == TGPost.PostType.LOCATION.getNumVal()
 				&& post.getNote().length() > 0) {
+			setTimeInclude(convertView, post);
 			TextView tvPostNote = (TextView) convertView
 					.findViewById(R.id.tvPostNote);
 			if (post.getNote() != null && post.getNote().length() > 0) {
@@ -205,10 +198,11 @@ public class StoryPostAdapter extends ArrayAdapter<TGPost> {
 			//do nothing
 		}
 		if (type == TGPost.PostType.REFERENCEDSTORY.getNumVal()) {
+			setTimeInclude(convertView, post);
 			setReferenceStoryAttributes(convertView, post.getReferencedStory());
 		}
 
-		if (post.getStory().isDraft()) {
+		if (post.getStory().isDraft() && post.getType() != PostType.REFERENCEDSTORY) {
 			convertView.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
@@ -220,9 +214,40 @@ public class StoryPostAdapter extends ArrayAdapter<TGPost> {
 		return convertView;
 	}
 
+	private void setTimeInclude(View convertView, TGPost post) {
+		TextView tvPostDateTime = (TextView) convertView
+				.findViewById(R.id.tvPostDateTime);
+		tvPostDateTime.setText(post.getFormattedCreateTime());
+	}
+
 	private void setReferenceStoryAttributes(View convertView,
 			TGStory referencedStory) {
-		
+		TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+		tvTitle.setText(referencedStory.getTitle());
+		ImageView ivCoverPhoto = (ImageView) convertView.findViewById(R.id.ivCoverPhoto);
+		ivCoverPhoto.setImageResource(android.R.color.transparent);
+		final ProgressBar pbLoading = (ProgressBar) convertView.findViewById(R.id.pbLoading);
+		pbLoading.setVisibility(ProgressBar.VISIBLE);
+		imageLoader.displayImage(referencedStory.getCoverPhotoURL(),
+				ivCoverPhoto, new SimpleImageLoadingListener() {
+
+					@Override
+					public void onLoadingStarted(String imageUri, View view) {
+						 pbLoading.setVisibility(ProgressBar.VISIBLE);
+					}
+
+					@Override
+					public void onLoadingFailed(String imageUri, View view,
+							FailReason failReason) {
+						pbLoading.setVisibility(ProgressBar.INVISIBLE);
+					}
+
+					@Override
+					public void onLoadingComplete(String imageUri, View view,
+							Bitmap loadedImage) {
+						pbLoading.setVisibility(ProgressBar.INVISIBLE);
+					}
+				});
 	}
 
 	private LayoutParams getLayoutParams() {
